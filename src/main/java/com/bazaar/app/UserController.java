eea2dac86214a4f9f104b210c137d4511283c71c
Sowndharya13 <sowndharyakanakaraj@gmail.com>
2020-08-09 20:26:46 +0530

    Added rest getmapping

--- src/main/java/com/bazaar/app/UserController.java
+++ src/main/java/com/bazaar/app/UserController.java
@@ -1,9 +1,12 @@
 package com.bazaar.app;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class UserController {
@@ -18,4 +21,19 @@ public class UserController {
         return service.getAllUsers();
 
     }
+
+    @GetMapping("/user/{user_id}")
+    public ResponseEntity<Users> get(@PathVariable Integer user_id) {
+        try {
+            Users users = service.get(user_id);
+            return new ResponseEntity<Users>(users, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Users>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+//    @PostMapping("/user")
+//    public void add(@RequestBody Users user) {
+//        service.save(user);
+//    }
 }
